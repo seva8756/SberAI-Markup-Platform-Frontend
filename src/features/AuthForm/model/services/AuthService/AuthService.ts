39e212de1068aca99ff7ea7fb8787f9a4021b36e
Daniel <negativ_2002@mail.ru>
2023-08-16 14:06:41 +0300

    fix: register fields name

--- src/features/AuthForm/model/services/AuthService/AuthService.ts
+++ src/features/AuthForm/model/services/AuthService/AuthService.ts
@@ -5,8 +5,13 @@ export default class AuthService {
   static async login(email: string, password: string) {
     return $api.post<User>('users/login', { email, password })
   }
-  static async registration(email: string, password: string, firstName: string, lastName: string) {
-    return $api.post<User>('users/create', { email, password, firstName, lastName })
+  static async registration(
+    email: string,
+    password: string,
+    first_name: string,
+    last_name: string
+  ) {
+    return $api.post<User>('users/create', { email, password, first_name, last_name })
   }
   static async logout() {
     return $api.post('users/logout')
